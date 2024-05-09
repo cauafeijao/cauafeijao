c59802aaf238ea252a9e314301832bc3d96ba953
cauazinho_cm <110676745+cauafeijao@users.noreply.github.com>
2024-05-09 07:42:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".botoes");
+const contadores = document.querySelectorAll(".contador")
+const tempoobijetivo1 = new Date("2024-05-10t00:00:00");
+
+contadores[0].textContent = tempoobijetivo1;
 
 for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
         for(j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
